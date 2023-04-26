b68bc5c32a1381d94551d01f550267756054e4c4
Avrohom Yisroel Silver <mryossu@hotmail.com>
2023-04-26 21:39:15 +0100

    Added Top and Left to CustomerDisplay to keep the windows in the right positions

--- TelerikWindowPlay/Data/CustomerDisplay.cs
+++ TelerikWindowPlay/Data/CustomerDisplay.cs
@@ -2,6 +2,19 @@
 
 public class CustomerDisplay : Customer {
   public bool Visible { get; set; }
+  private int _top = 500;
+
+  public string TopStr {
+    set => _top = Convert.ToInt32(value.Replace("px", ""));
+    get => $"{_top}px";
+  }
+
+  private int _left = 500;
+
+  public string LeftStr {
+    set => _left = Convert.ToInt32(value.Replace("px", ""));
+    get => $"{_left}px";
+  }
 
   public CustomerDisplay(int id, string firstName, string surname, string email, string address, string city, string postcode) : base(id, firstName, surname, email, address, city, postcode) {
   }
